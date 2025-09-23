9d6a72e5943f2ec1ef6fc572205acd88c1d7680c
AlieniAgustin <alieniagustinweb@gmail.com>
2025-09-23 10:34:09 -0300

    add user schema

--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -1,1 +1,22 @@
-#todo
+from pydantic import BaseModel, EmailStr
+from enum import Enum
+from typing import Optional
+from datetime import datetime
+
+
+# enum for role 
+class UserRole(str, Enum):
+    DRIVER = "driver"
+    PASSENGER = "passenger"
+
+# schema to serialize/validate users 
+class UserSchema(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    dni: str
+    email: EmailStr
+    role: UserRole
+
+    class Config:
+        orm_mode = True  # allows you to receive SQLAlchemy objects directly
