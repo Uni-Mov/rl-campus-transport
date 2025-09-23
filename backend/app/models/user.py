27b0b34b2e8491e76bb3c28483dc5e789979e574
AlieniAgustin <alieniagustinweb@gmail.com>
2025-09-23 10:21:36 -0300

    modify user model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -15,14 +15,10 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
+    dni = Column(String(10), unique=True, index=True, nullable=False)
     email = Column(String(120), unique=True, index=True, nullable=False)
     password_hash = Column(String(255), nullable=False)
-
     role = Column(Enum(UserRole), nullable=False)
-
-    last_login = Column(DateTime(timezone=True), nullable=True)
-
-    is_active = Column(Boolean, default=True)
 
     def __repr__(self): #defines how an object is represented as a string 
         return (
