0be1dbccf72710f9887b221fdc03d13a4f6ca107
Francisco Natale <francisco_natale@hotmail.com>
2025-10-03 22:51:09 -0300

    delete route example of app and main

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "../pages/home/Home"
-import Clients from "../pages/clients/Clients"
 import Login from "../pages/login/Login"
 import Header from "../components/organisms/Header/Header"
 import Footer from "../components/organisms/Footer"
@@ -15,7 +14,6 @@ export default function App() {
       <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/clients" element={<Clients />} />
           <Route path="/login" element={<Login />}/>
           <Route path="/register" element={<Register />}/>
           <Route path="/contact" element={<Contact />}/>
