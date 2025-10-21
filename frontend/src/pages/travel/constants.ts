af8abce425a68a02d388434840a5a70dd13fd151
Francisco Natale <francisco_natale@hotmail.com>
2025-10-21 20:13:34 -0300

    add constant for university

---
+++ frontend/src/pages/travel/constants.ts
@@ -0,0 +1,5 @@
+import type { Coordinate } from "@/types/coordinate";
+
+// Coordenadas de la Universidad Nacional de Río Cuarto
+export const UNIVERSITY_COORDINATES: Coordinate = [-64.3005, -33.1095];
+
