0df6d94877d81784ddf0a79ef0dc70a5e6f67fe6
Francisco Natale <148393465+francisconatale@users.noreply.github.com>
2025-10-23 20:19:09 -0300

    minor change in contact

--- frontend/src/pages/contact/Contact.tsx
+++ frontend/src/pages/contact/Contact.tsx
@@ -3,15 +3,6 @@ import Member from "./organisms/Member"
 import MemberContainer from "./organisms/MemberContainer"
 
 export default function Contact() {
-  const members = [
-    { name: "Francisco Natale", role: "Frontend developer", avatar: "https://github.com/sbfrancisco.png", github: "https://github.com/sbfrancisco" },
-    { name: "Franco Vesnaver", role: "Frontend developer", avatar: "https://github.com/FranVesnaver.png", github: "https://github.com/FranVesnaver" },
-    { name: "Valentino Vilar", role: "IA-ML developer", avatar: "https://github.com/valenvilar5.png", github: "https://github.com/valenvilar5" },
-    { name: "Francisco Barosco", role: "IA-ML developer", avatar: "https://github.com/franBarosco.png", github: "https://github.com/franBarosco" },
-    { name: "Agustin Alieni", role: "Backend developer", avatar: "https://github.com/AlieniAgustin.png", github: "https://github.com/AlieniAgustin" },
-    { name: "Hernan Jara", role: "Backend developer", avatar: "https://github.com/HernanJara4.png", github: "https://github.com/HernanJara4" },
-  ]
-
   return (
     <div className="max-w-4xl mx-auto">
       <HeaderContact />
