2f706e26e2fcfecc218ff81b8ce438ae95fc5212
FranVesnaver <fevesnaver@gmail.com>
2025-11-02 19:07:15 -0300

    add a method to get current session user data in the frontend

--- frontend/src/context/auth-context.tsx
+++ frontend/src/context/auth-context.tsx
@@ -1,5 +1,6 @@
 import { createContext, useContext } from "react"
 import { useAuth as useAuthHook } from "../hooks/useAuth"
+import type { User } from "@/models/user";
 
 type AuthContextType = {
   isLoggedIn: boolean;
@@ -7,6 +8,7 @@ type AuthContextType = {
   login: (email : string, password: string) => void;
   logout: () => void;
   register: (name: string, lastname: string, dni: string, email: string, password: string, role: string) => void;
+  getUserFromToken: () => Promise<User>;
 };
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { login as apiLogin, logout as apiLogout, register as apiRegister } from "@/api/auth-api";
+import type { User } from "@/models/user"
+import { jwtDecode } from "jwt-decode"; 
 
 export const useAuth = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -44,7 +46,21 @@ export const useAuth = () => {
         navigate("/travel");
     };
 
-    return { isLoggedIn, loading, login, logout, register };
+    const getUserFromToken = async (): Promise<User> => {
+        // esto espera una token JWT, pero el backend no manda un JSON normal
+        // cambiar el backend para que mande un JWT
+        if (isLoggedIn) {
+            const token = localStorage.getItem("authToken");
+            if (!token) {
+                throw new Error("No token found");
+            }
+            const decoded = jwtDecode<User>(token);
+            return decoded;
+        }
+        throw new Error("No user logged in");
+    }
+
+    return { isLoggedIn, loading, login, logout, register, getUserFromToken };
 };
 
  
