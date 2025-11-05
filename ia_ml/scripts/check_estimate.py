e4fde5d8d5c69c4c00720f0e91ecc1ae8fdba33f
franBarosco <franbarosco@gmail.com>
2025-11-05 17:33:03 -0300

    update gitignore

--- ia_ml/scripts/check_estimate.py
+++
@@ -1,8 +0,0 @@
-
-from ia_ml.utils.estimate_training_time import estimate
-
-num_episodes = 1000
-steps_per_episode = 500
-
-time_estimate = estimate(num_episodes, steps_per_episode)
-print(f"Tiempo estimado de entrenamiento: {time_estimate:.2f} minutos") 
--- ia_ml/scripts/quick_viz.py
+++
@@ -1,8 +0,0 @@
-from ia_ml.utils.see_nodes import plot_nodes
-from ia_ml.utils.see_streets import plot_streets
-
-graph_file = "src/data/Río_Cuarto_Cordoba_Argentina.graphml"
-
-plot_nodes(graph_file)
-plot_streets(graph_file)
-print("Visualización generada.")
